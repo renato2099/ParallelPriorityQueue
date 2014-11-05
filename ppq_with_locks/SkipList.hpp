1e84bc0dc947ffb98c360f06102843138b526fef
David Sidler <david.sidler@gmail.com>
2014-11-05 12:11:26 +0100

    Added template support to PPQ & Skiplist

--- ppq_with_locks/SkipList.hpp
+++ ppq_with_locks/SkipList.hpp
@@ -14,42 +14,181 @@
 
 using namespace std;
 
-typedef struct Node
+template <class T> class Node
 {
-	/* priority */
-	uint64_t key;
-
+public:
 	/* data */
-	void *data;
+	T	data;
 
 	/* level of the node */
 	int level;
 
 	/* pointers to the next nodes */
 	struct Node *forward[MAX_LEVEL];
-} Node;
+};
 
-class SkipList
+template <class T> class SkipList
 {
 	private:
 	int level;
-	Node *head;
+	Node<T> *head;
 
 #ifdef THREAD_SAFE
 	mutex mtx;
 #endif
 
 	public:
-	SkipList();
-	~SkipList();
-	void insert(void *data, uint64_t key);
-	void insert(void *data[], uint64_t key[], int k);
-	void *find(uint64_t key);
-	void remove(uint64_t key);
-	void *pop_front();
-	void **pop_front(int k);
-	void print();
-	void printLevel(int l);
+	SkipList()
+	{
+	   srand(SEED);
+		level = 0;
+		head = new Node<T>();
+		if (head == NULL)
+		{
+			exit(1);
+		}
+		for (int i = 0; i < MAX_LEVEL; i++)
+		{
+			head->forward[i] = NULL;
+		}
+	};
+	~SkipList()
+	{
+		Node<T> *tmp, *p;
+		p =  head->forward[0];
+		while (p != NULL)
+		{
+			tmp = p;
+			p = p->forward[0];
+			delete tmp;
+		}
+		//delete tmp; //TODO is this necessary?
+	};
+	void insert(T data)
+	{
+		int min_level;
+		Node<T> *p, *q, *update[MAX_LEVEL];
+
+		q = new Node<T>();
+		if (q == NULL)
+		{
+			exit(1);
+		}
+		q->data = data;
+#ifdef THREAD_SAFE
+		mtx.lock();
+#endif
+		/* gets a random level for the new node
+	 	* its level may exceed the current level of the list, at most, by 1
+	 	*/
+		q->level = 0;
+		while (q->level < MAX_LEVEL && q->level <= this->level && ((float) rand() / RAND_MAX) < P)
+		{
+			q->level++;
+		}
+
+		/* starts from the current top level and keeps track of the places
+	 	* where the search drops down one level, by storing their address in an array
+	 	* this places are required to connect the new node in the skip list
+	 	* the update[0] will point to the node with the greatest key that does not exceed
+	 	* the key of the new node, thus the new node is inserted immediately after that
+	 	*/
+		p = this->head;
+		for (int i = this->level; i >= 0; i--)
+		{
+			while (p->forward[i] != NULL && p->forward[i]->data < data)
+			{
+				p = p->forward[i];
+			}
+			update[i] = p;
+		}
+
+   	 /* inserts the new node in the skip list and updates the pointers
+	 	*/
+		min_level = (q->level < this->level) ? q->level : this->level;
+    	for (int i = 0; i <= min_level; i++)
+		{
+			q->forward[i] = update[i]->forward[i];
+			update[i]->forward[i] = q;
+    	}
+
+		/* if the level of the new node exceeds the current level of the skip list
+	 	* then the current skip list level is updated and
+	 	* the top level of the head has to point to the new node
+	 	* and the top level of the new node to null
+	 	*/ 
+		if (this->level < q->level)
+		{
+			this->level = q->level;
+			this->head->forward[q->level] = q;
+			q->forward[q->level] = NULL;
+		}
+
+#ifdef THREAD_SAFE
+		mtx.unlock();
+#endif
+	};
+	void insert(T *data[], int k)
+	{
+		for (int i = 0; i < k; i++)
+		{
+			insert(data[i]);
+		}
+	}
+	void *find(T data) { return NULL; };
+	void remove(T data) {};
+	T    pop_front()
+	{
+		Node<T> *p;
+#ifdef THREAD_SAFE
+		mtx.lock();
+#endif
+		p = head->forward[0];
+		if (p != NULL)
+		{
+			for (int i = 0; i <= p->level; i++)
+			{
+				head->forward[i] = p->forward[i];
+			}
+#ifdef THREAD_SAFE
+			mtx.unlock();
+#endif
+			return p->data;
+		}
+#ifdef THREAD_SAFE
+		return NULL;
+#endif
+	};
+	T*	  pop_front(int k)
+	{
+		for (int i = 0; i < k; i++)
+		{
+			pop_front();
+		}
+		return NULL;
+	}
+	void print()
+	{
+		Node<T> *p;
+		// the skip list is traversed at the lowest level
+		p = head->forward[0];
+		while (p != NULL)
+		{
+			cout << "--" << p->data << ", (" << p->level << ")--" << endl;
+			p = p->forward[0];
+		}
+	}
+	void printLevel(int l)
+	{
+		Node<T> *p;
+		// the skip list is traversed by level
+		p = head->forward[l];
+		while(p != NULL)
+		{
+			cout << p->data << endl;
+			p = p->forward[l];
+		}
+	}
 };
 
 #endif
--- ppq_with_locks/main.cpp
+++ ppq_with_locks/main.cpp
@@ -7,16 +7,19 @@
 
 using namespace std;
 
+#define THREAD_SAFE 1
+
 #define THREADS 10
 
-void thread_routine(int id, PPQ *ppq)
+void thread_routine(int id, PPQ<int> *ppq)
 {
-	int i, data[10];
+	int i;//, data[10];
 
 	for (i = 0; i < 10; i++)
 	{
-		data[i] = id * 10 + i + 1;
-		ppq->insert((void *) &data[i], 10 * (id * 10 + i + 1));
+		//data[i] = id * 10 + i + 1;
+		//ppq->insert((void *) &data[i], 10 * (id * 10 + i + 1));
+		ppq->insert(10 * (id * 10 + i + 1));
 	}
 
 	for (i = 1; i < 10; i++)
@@ -28,10 +31,10 @@ void thread_routine(int id, PPQ *ppq)
 int main()
 {
 	int i;
-	PPQ *ppq;
+	PPQ<int> *ppq;
 	thread tid[THREADS];
 
-	ppq = new PPQ();
+	ppq = new PPQ<int>();
 
 	for (i = 0; i < THREADS; i++)
 	{
