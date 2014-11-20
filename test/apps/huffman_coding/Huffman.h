c4362fda6b333cfe0d259fb2d965df771082b32f
Renato Marroquin <renatoj.marroquin@gmail.com>
2014-11-20 22:37:20 +0100

    Cleaning up

---
+++ test/apps/huffman_coding/Huffman.h
@@ -0,0 +1,133 @@
+#include "MinHeap.h"
+
+#ifndef HUFFMAN_H
+#define HUFFMAN_H
+
+#define MAX_TREE_HT 100
+
+priority_queue_mhn_type* createAndBuildStdMinHeap(const char* data, int freq[], int size)
+{
+    priority_queue_mhn_type *pq = new priority_queue_mhn_type;
+    for (int i = 0; i < size; ++i)
+        pq->push(new InnerNode(newNode(data[i], freq[i])));
+	
+	return pq;
+}
+
+priority_queue_mhn_type* buildHuffmanStdTree(const char* data, int freq[], int size)
+{
+    struct MinHeapNode *top;
+    // Step 1: Create a min heap of capacity equal to size.  Initially, there are
+    // modes equal to size.
+    priority_queue_mhn_type *pq = createAndBuildStdMinHeap(data, freq, size);
+	
+    // Iterate while size of heap doesn't become 1
+    while (pq->size() != 1)
+    {
+
+        // Step 2: Extract the two minimum freq items from min heap
+        InnerNode* l = pq->top();
+        pq->pop();
+        InnerNode* r = pq->top();
+        pq->pop();
+		
+        // Step 3:  Create a new internal node with frequency equal to the
+        // sum of the two nodes frequencies. Make the two extracted node as
+        // left and right children of this new node. Add this node to the min heap
+        // '$' is a special value for internal nodes, not used
+        top = newNode('$', l->getNode().freq + r->getNode().freq);
+        top->left = l->getRef();
+        top->right = r->getRef();
+	
+        pq->push(new InnerNode(top));
+    }
+
+    // Step 4: The remaining node is the root node and the tree is complete.
+    return pq;
+}
+
+// The main function that builds Huffman tree
+struct MinHeapNode* buildHuffmanTree(const char* data, int freq[], int size)
+{
+    struct MinHeapNode *left, *right, *top;
+ 
+    // Step 1: Create a min heap of capacity equal to size.  Initially, there are
+    // modes equal to size.
+    struct MinHeap* minHeap = createAndBuildMinHeap(data, freq, size);
+ 
+    // Iterate while size of heap doesn't become 1
+    while (!isSizeOne(minHeap))
+    {
+        // Step 2: Extract the two minimum freq items from min heap
+        left = extractMin(minHeap);
+        right = extractMin(minHeap);
+ 
+        // Step 3:  Create a new internal node with frequency equal to the
+        // sum of the two nodes frequencies. Make the two extracted node as
+        // left and right children of this new node. Add this node to the min heap
+        // '$' is a special value for internal nodes, not used
+        top = newNode('$', left->freq + right->freq);
+        top->left = left;
+        top->right = right;
+        insertMinHeap(minHeap, top);
+    }
+ 
+    // Step 4: The remaining node is the root node and the tree is complete.
+    return extractMin(minHeap);
+}
+ 
+// Prints huffman codes from the root of Huffman Tree.  It uses arr[] to
+// store codes
+void printCodes(MinHeapNode* root, int arr[], int top)
+{
+    // Assign 0 to left edge and recur
+    if (root->left)
+    {
+        arr[top] = 0; 
+        printCodes(root->left, arr, top + 1);
+    }
+    // Assign 1 to right edge and recur
+    if (root->right)
+    {
+        arr[top] = 1;
+        printCodes(root->right, arr, top + 1);
+    }
+    // If this is a leaf node, then it contains one of the input
+    // characters, print the character and its code from arr[]
+    if (isLeaf(root))
+    {
+        printf("%c: ", root->data);
+        printArr(arr, top);
+    }
+}
+ 
+void printStdCodes(priority_queue_mhn_type *pq, int arr[], int top)
+ {    
+    InnerNode* root = pq->top();
+	MinHeapNode  rn = root->getNode();
+    printCodes(&rn, arr, top);
+    pq->pop(); 
+ }
+
+// The main function that builds a Huffman Tree and print codes by traversing
+// the built Huffman Tree
+void HuffmanCodes(const char *data, int freq[], int size)
+{
+   //  Construct Huffman Tree
+   struct MinHeapNode* root = buildHuffmanTree(data, freq, size);
+ 
+   // Print Huffman codes using the Huffman tree built above
+   int arr[MAX_TREE_HT], top = 0;
+   printCodes(root, arr, top);
+}
+
+void HuffmanCodesStd(const char *data, int freq[], int size)
+{
+    //  Construct Huffman Tree
+    priority_queue_mhn_type *pqueue = buildHuffmanStdTree(data, freq, size);
+    // Print Huffman codes using the Huffman tree built above
+    int arr[MAX_TREE_HT], top = 0;
+    printStdCodes(pqueue, arr, top);
+}
+
+#endif
--- test/apps/huffman_coding/MinHeapNode.h
+++ test/apps/huffman_coding/MinHeapNode.h
@@ -7,20 +7,7 @@
 #ifndef MINHEAPNODE_H
 #define MINHEAPNODE_H
 
-class Int {
-public:
-    Int(int val) : m_val(val) {}
-    int getVal() { return m_val; }
-    bool operator<(const Int &other) const { return m_val < other.m_val; }
-private:
-    int m_val;
-};
-
-template<typename Type, typename Compare = std::less<Type> >
-struct pless : public std::binary_function<Type *, Type *, bool> {
-    bool operator()(const Type *x, const Type *y) const
-        { return Compare()(*x, *y); }
-};
+using namespace std; 
 
 // A Huffman tree node
 struct MinHeapNode
@@ -33,13 +20,13 @@ struct MinHeapNode
 class InnerNode
 {
 public:
-	InnerNode(MinHeapNode in) : node(in) {}
-	InnerNode(MinHeapNode* in) : node(*in) {}
-	MinHeapNode getNode() {return node;}
-	MinHeapNode* getRef() {return &node;}
-	bool operator<(const InnerNode &other) const { return node.freq > other.node.freq; }
-	MinHeapNode node;
-	private:
+    InnerNode(MinHeapNode in) : node(in) {}
+    InnerNode(MinHeapNode* in) : node(*in) {}
+    MinHeapNode getNode() {return node;}
+    MinHeapNode* getRef() {return &node;}
+    bool operator<(const InnerNode &other) const { return node.freq > other.node.freq; }
+    MinHeapNode node;
+    private:
 };
 
 template<typename Type, typename Compare = std::less<Type> >
@@ -47,5 +34,7 @@ struct nless : public std::binary_function<Type *, Type *, bool> {
     bool operator()(const Type *x, const Type *y) const
         { return Compare()(*x, *y); }
 };
+
+typedef priority_queue<InnerNode*, vector<InnerNode*>, nless<InnerNode> >priority_queue_mhn_type;
 
 #endif
