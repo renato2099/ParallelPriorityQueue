7fd5925635330420a4d162f4806186b3d5cd3acf
Renato Marroquin <renatoj.marroquin@gmail.com>
2014-12-09 13:17:03 +0100

    Clean up

--- ppq_tbb/SkipList.hpp
+++ ppq_tbb/SkipList.hpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <stdint.h>
-#include "tbb/atomic.h"
-#include "AtomicRef.hpp"
+#include <atomic>
+#include <thread>
+#include "../inc/AtomicRef.hpp"
 
 #ifndef SKIPLIST_HPP
 #define SKIPLIST_HPP
 
-#define SEED		10
-#define MAX_LEVEL	21
-#define PROB		0.5
+#define SEED				10
+#define MAX_LEVEL			21
+#define PROB				0.5
+
+#define INSERT_THREADS		2
+#define INSERT_THRESHOLD	100
 
 using namespace std;
 
@@ -16,8 +20,8 @@ using namespace std;
 
 template <typename T>  struct Node
 {	
-	T		data;
-	int		level;
+	T data;
+	int	level;
 	AtomicRef<Node>	next[MAX_LEVEL];
 };
 
@@ -25,50 +29,48 @@ template <class T, class Comparator>
 class SkipList
 {
 	private:
-	std::atomic<size_t>				mSize;
-	std::atomic<int>					mLevel;
-	Node<T>*						head;
+	std::atomic<size_t>	mSize;
+	std::atomic<int> mLevel;
+	Node<T> *head;
 	//friend bool comparator_ <T,Comparator> (const T& t1, const T& t2);
 	bool comp(const T& t1, const T& t2) { return Comparator()(t1, t2); };
-	bool equal(const T& t1, const T& t2) { return (Comparator()(t1,t2) == Comparator()(t2,t1)); }; //TODO get rid of this
-
+	bool equal(const T& t1, const T& t2) { return (Comparator()(t1,t2) == Comparator()(t2,t1)); }; 
 
 	public:
+	SkipList* me;
 	SkipList();
 	~SkipList();
 	bool empty() const;
 	size_t size() const;
 	bool insert(const T& data);
-	/*void insert(T *data[], int k);
-	T    find(T data);*/
+	size_t insert(T data[], int k);
+	/*T find(T data);*/
 	bool remove(const T& data);
-	bool    pop_front(T& data);
-	size_t	  pop_front(T data[], int k);
+	bool pop_front(T& data);
+	size_t pop_front(T data[], int k);
 	void print();
 	//void printLevel(int l);
-private:
+
+	private:
 	bool findNode(const T& data, Node<T>** preds, Node<T>** succs);
+	static void insert_aux_thread(SkipList<T,Comparator> *mthis, T data[], int k, int *inserted);
 };
 
 /*
  * Implementation of template class
  */
 
-
 template<class T, class Comparator>
 SkipList<T,Comparator>::SkipList()
 {
-   srand(SEED);
+	srand(SEED);
 	mLevel = 0; // rename level and head
 	mSize = 0;
+
 	//TODO initialize head;
 	head = new Node<T>();
-	head->level = MAX_LEVEL;
-	/*for (int i = 0; i < MAX_LEVEL-1; i++)
-	{
-		head->next[i] = nullptr;//maybe to in Node class
-	}*/
-};
+	head->level = MAX_LEVEL-1;
+}
 
 template<class T, class Comparator>
 SkipList<T,Comparator>::~SkipList()
@@ -84,7 +86,7 @@ SkipList<T,Comparator>::~SkipList()
 		delete tmp;
 	}
 	delete head;
-};
+}
 
 //TODO have to clean up
 template<class T, class Comparator>
@@ -96,15 +98,15 @@ bool SkipList<T, Comparator>::findNode(const T& data, Node<T>** preds, Node<T>**
 	Node<T>* pred = nullptr;
 	Node<T>* curr = nullptr;
 	Node<T>* succ = nullptr;
+
 	retry:
 	while(true) 
 	{
-
 		pred = this->head;
-		for (int level = MAX_LEVEL-1; level >= bottomLevel; level--)
+		for (int level = mLevel; level >= bottomLevel; level--)
 		{
 			curr = pred->next[level].getRef();
-			while(true)
+			while (true)
 			{
 				//If there is not a successor then stop
 				if (curr == nullptr)
@@ -113,7 +115,7 @@ bool SkipList<T, Comparator>::findNode(const T& data, Node<T>** preds, Node<T>**
 				marked = curr->next[level].getMarked();
 				succ = curr->next[level].getRef();
 				// check if the node is marked
-				while(marked)
+				while (marked)
 				{
 					snip = pred->next[level].compareAndSet(curr, succ, false, false);
 					if (!snip) 
@@ -144,7 +146,7 @@ bool SkipList<T, Comparator>::findNode(const T& data, Node<T>** preds, Node<T>**
 					break;
 				}
 			}
-			preds[level] = pred; //TODO replace with pointer array
+			preds[level] = pred;
 			succs[level] = curr;
 		}
 		if (curr != NULL)
@@ -152,6 +154,7 @@ bool SkipList<T, Comparator>::findNode(const T& data, Node<T>** preds, Node<T>**
 		else 
 			return false;
 	}
+
 	return false;
 }
 
@@ -168,40 +171,45 @@ size_t SkipList<T,Comparator>::size() const
 	return mSize;
 }
 
-
 template<class T, class Comparator>
 bool SkipList<T,Comparator>::insert(const T& data)
 {
 	int topLevel = 0;
+	int bottomLevel = 0;
+	Node<T>** preds = new Node<T>*[MAX_LEVEL];
+	Node<T>** succs = new Node<T>*[MAX_LEVEL];
+	Node<T>*  nnode = nullptr;
 	while (topLevel < (MAX_LEVEL - 1) && topLevel <= mLevel && ((float) rand() / RAND_MAX) < PROB)
 	{
 		topLevel++;
 	}
-
 	if (topLevel > mLevel)
 	{
 		mLevel = topLevel;
-	}	
-	int bottomLevel = 0;
-	Node<T>** preds = new Node<T>*[MAX_LEVEL+1];
-	Node<T>** succs = new Node<T>*[MAX_LEVEL+1];
-	
-	while(true)
+	}
+	while (true)
 	{
-		bool found = findNode(data, preds, succs);
-		if (found)
+		findNode(data, preds, succs);
+		/*if (found)
 		{
+			delete[] preds;
+			delete[] succs;
 			return false;
 		}
-		else
+		else*/
 		{
-
-			Node<T>* nnode = new Node<T>();
+			if (nnode == nullptr)
+			{
+				nnode = new Node<T>();
+			}
 			nnode->data = data;
 			nnode->level = topLevel;
 			
+			//FIXME Could we move this for loop after the "compareAndSet"?
+			//FIXME the difference is when it fails, because we don't perform useless operations
 			// The new node gets the reference from the successor
-			for(int level = bottomLevel; level <=topLevel; level++)
+			// I would leave it like this, probably doesn't matter
+			for (int level = bottomLevel; level <=topLevel; level++)
 			{
 				Node<T>* succ = succs[level];
 				nnode->next[level].setRef(succ, false);
@@ -210,16 +218,15 @@ bool SkipList<T,Comparator>::insert(const T& data)
 			Node<T>* pred = preds[bottomLevel];
 			Node<T>* succ = succs[bottomLevel];
 
-			nnode->next[bottomLevel].setRef(succ, false);
-
 			if (!(pred->next[bottomLevel].compareAndSet(succ, nnode, false, false)))
 			{
 				continue;
-			}	
+			}
+
 			// Update predecessors
-			for(int level = bottomLevel+1; level <= topLevel; level++)
+			for (int level = bottomLevel+1; level <= topLevel; level++)
 			{
-				while(true)
+				while (true)
 				{
 					pred = preds[level];
 					succ = succs[level];
@@ -231,38 +238,414 @@ bool SkipList<T,Comparator>::insert(const T& data)
 				}
 			}
 			mSize++;
+			delete[] preds;
+			delete[] succs;
 			return true;
 		}
+		//this is actually never executed
 		break;
 	}
+	delete[] preds;
+	delete[] succs;
 	return false;
+}
 
-};
+template<class T, class Comparator>
+void SkipList<T,Comparator>::insert_aux_thread(SkipList<T,Comparator> *mthis, T data[], int k, int *inserted)
+{
+	int bottomLevel = 0;
+	int count, n, batch_maxLevel, next;
+	Node<T>** preds = new Node<T> *[MAX_LEVEL];
+	Node<T>** succs = new Node<T> *[MAX_LEVEL];
+
+	int *batch_topLevel = new int[k];
+
+	batch_maxLevel = mthis->mLevel;
+	for (int i = 0; i < k; i++)
+	{
+		batch_topLevel[i] = 0;
+		while (batch_topLevel[i] < (MAX_LEVEL - 1) && batch_topLevel[i] <= batch_maxLevel && ((float) rand() / RAND_MAX) < PROB)
+		{
+			batch_topLevel[i]++;
+		}
+
+		if (batch_topLevel[i] > batch_maxLevel)
+		{
+			batch_maxLevel = batch_topLevel[i];
+		}
+	}
+
+	count = 0;
+	*inserted = 0;
+	while (count < k)
+	{
+		bool found = mthis->findNode(data[count], preds, succs);
+		if (found)
+		{
+			/* if one element is already in the SkipList, we ignore it */
+			count++;
+		}
+		else
+		{
+			/* we want to compute the number of elements that have to be inserted in this position
+			 * i.e. their key < succ->key
+			 * if succ[0] is the nullptr, all elements have to be inserted, because I am at the end
+			 * we store this number in "n" variable
+			 */
+			if (succs[0] != nullptr)
+			{
+				n = count;
+				while (n < k && mthis->comp(data[n], succs[0]->data))
+				{
+					n++;
+				}
+				n -= count;
+			}
+			else
+			{
+				n = k - count;
+			}
+
+			Node<T> **nnode = new Node<T> *[n]();
+			for (int i = 0; i < n; i++)
+			{
+				nnode[i] = new Node<T>();
+			}
+
+			batch_maxLevel = 0;
+			for (int i = 0; i < n; i++)
+			{
+				nnode[i]->data = data[count + i];
+				nnode[i]->level = batch_topLevel[*inserted + i];
+
+				if (nnode[i]->level > batch_maxLevel)
+				{
+					batch_maxLevel = nnode[i]->level;
+				}
+			}
+
+			/* we link all the nodes */
+			for (int i = 0; i < n - 1; i++)
+			{
+				for (int level = bottomLevel; level <= nnode[i]->level; level++)
+				{
+					/* we have to look for the next node with the appropriate level */
+					next = i + 1;
+					while (next < n && nnode[next]->level < level)
+					{
+						next++;
+					}
+
+					Node<T> *succ = (next == n) ? succs[level] : nnode[next];
+					nnode[i]->next[level].setRef(succ, false);
+				}
+			}
+
+			/* we link last of the "n" nodes to the succs */
+			for (int level = bottomLevel; level <= nnode[n - 1]->level; level++)
+			{
+				Node<T>* succ = succs[level];
+				nnode[n - 1]->next[level].setRef(succ, false);
+			}
+
+			Node<T> *pred = preds[bottomLevel];
+			Node<T> *succ = succs[bottomLevel];
+
+			if (!(pred->next[bottomLevel].compareAndSet(succ, nnode[0], false, false)))
+			{
+				for (int i = 0; i < n; i++)
+				{
+					delete nnode[i];
+				}
+
+				delete [] nnode;
+
+				continue;
+			}
+
+			/* we can now update the level of the SkipList, based ONLY on the level of the current batch */
+			if (batch_maxLevel > mthis->mLevel)
+			{
+				mthis->mLevel = batch_maxLevel;
+			}
+
+			for (int level = bottomLevel + 1; level <= batch_maxLevel; level++)
+			{
+				/* we have to look for the node with the appropiate level again
+				 * but not inside the next while loop
+				 */
+				next = 0;
+				while (next < n && nnode[next]->level < level)
+				{
+					next++;
+				}
+
+				while (true)
+				{
+					pred = preds[level];
+					succ = succs[level];
+					if (pred->next[level].compareAndSet(succ, nnode[next], false, false))
+					{
+						break;
+					}
+					mthis->findNode(data[count], preds, succs);
+				}	
+			}
+
+			count += n;
+			*inserted += n;
+
+			delete [] nnode;
+		}
+	}
+
+	delete [] preds;
+	delete [] succs;
+	delete [] batch_topLevel;
+}
+
+template<class T, class Comparator>
+size_t SkipList<T,Comparator>::insert(T data[], int k)
+{
+	int bottomLevel = 0;
+	int count, inserted, n, batch_maxLevel, next;
+	Node<T>** preds = new Node<T> *[MAX_LEVEL];
+	Node<T>** succs = new Node<T> *[MAX_LEVEL];
+	
+	/* sorting */
+	for (int i = 0; i < k - 1; i++)
+	{
+		for (int j = 0; j < k - 1 - i; j++)
+		{
+			if (comp(data[j + 1], data[j]))
+			{
+				T temp = data[j + 1];
+				data[j + 1] = data[j];
+				data[j] = temp;
+			}
+		}
+	}
+
+	/* duplicates elimination */
+	for (int i = 0; i < k - 1; i++)
+	{
+		while (i + 1 < k && !comp(data[i], data[i + 1]))
+		{
+			for (int j = 0; j < k - 2 - i; j++)
+			{
+				data[i + 1 + j] = data[i + 2 + j];
+			}
+
+			k--;
+		}
+	}
+
+	if (k > INSERT_THRESHOLD)
+	{
+		delete [] preds;
+		delete [] succs;
+
+		thread tid[INSERT_THREADS];
+		int inserted_nodes[INSERT_THREADS];
+
+		for (int j = 0; j < INSERT_THREADS - 1; j++)
+		{
+			tid[j] = thread(insert_aux_thread,
+							(SkipList<T,Comparator> *) this,
+							&(data[j * (k / INSERT_THREADS)]),
+							k / INSERT_THREADS,
+							&(inserted_nodes[j]));
+		}
+
+		tid[INSERT_THREADS - 1] = thread(insert_aux_thread,
+										(SkipList<T,Comparator> *) this,
+										&(data[(INSERT_THREADS - 1) * (k / INSERT_THREADS)]),
+										k / INSERT_THREADS + k % INSERT_THREADS,
+										&(inserted_nodes[INSERT_THREADS - 1]));
+
+		inserted = 0;
+		for (int j = 0; j < INSERT_THREADS; j++)
+		{
+			tid[j].join();
+			inserted += inserted_nodes[j];
+		}
+
+		mSize += inserted;
+		return (inserted);
+	}
+
+	int *batch_topLevel = new int[k];
+
+	batch_maxLevel = mLevel;
+	for (int i = 0; i < k; i++)
+	{
+		batch_topLevel[i] = 0;
+		while (batch_topLevel[i] < (MAX_LEVEL - 1) && batch_topLevel[i] <= batch_maxLevel && ((float) rand() / RAND_MAX) < PROB)
+		{
+			batch_topLevel[i]++;
+		}
+
+		if (batch_topLevel[i] > batch_maxLevel)
+		{
+			batch_maxLevel = batch_topLevel[i];
+		}
+	}
+
+	count = 0;
+	inserted = 0;
+	while (count < k)
+	{
+		bool found = findNode(data[count], preds, succs);
+		if (found)
+		{
+			/* if one element is already in the SkipList, we ignore it */
+			count++;
+		}
+		else
+		{
+			/* we want to compute the number of elements that have to be inserted in this position
+			 * i.e. their key < succ->key
+			 * if succ[0] is the nullptr, all elements have to be inserted, because I am at the end
+			 * we store this number in "n" variable
+			 */
+			if (succs[0] != nullptr)
+			{
+				n = count;
+				while (n < k && comp(data[n], succs[0]->data))
+				{
+					n++;
+				}
+				n -= count;
+			}
+			else
+			{
+				n = k - count;
+			}
+
+			Node<T> **nnode = new Node<T> *[n]();
+			for (int i = 0; i < n; i++)
+			{
+				nnode[i] = new Node<T>();
+			}
+
+			batch_maxLevel = 0;
+			for (int i = 0; i < n; i++)
+			{
+				nnode[i]->data = data[count + i];
+				nnode[i]->level = batch_topLevel[inserted + i];
+
+				if (nnode[i]->level > batch_maxLevel)
+				{
+					batch_maxLevel = nnode[i]->level;
+				}
+			}
+
+			/* we link all the nodes */
+			for (int i = 0; i < n - 1; i++)
+			{
+				for (int level = bottomLevel; level <= nnode[i]->level; level++)
+				{
+					/* we have to look for the next node with the appropriate level */
+					next = i + 1;
+					while (next < n && nnode[next]->level < level)
+					{
+						next++;
+					}
+
+					Node<T> *succ = (next == n) ? succs[level] : nnode[next];
+					nnode[i]->next[level].setRef(succ, false);
+				}
+			}
+
+			/* we link last of the "n" nodes to the succs */
+			for (int level = bottomLevel; level <= nnode[n - 1]->level; level++)
+			{
+				Node<T>* succ = succs[level];
+				nnode[n - 1]->next[level].setRef(succ, false);
+			}
+
+			Node<T> *pred = preds[bottomLevel];
+			Node<T> *succ = succs[bottomLevel];
+
+			if (!(pred->next[bottomLevel].compareAndSet(succ, nnode[0], false, false)))
+			{
+				for (int i = 0; i < n; i++)
+				{
+					delete nnode[i];
+				}
+
+				delete [] nnode;
+
+				continue;
+			}
+
+			/* we can now update the level of the SkipList, based ONLY on the level of the current batch */
+			if (batch_maxLevel > mLevel)
+			{
+				mLevel = batch_maxLevel;
+			}
+
+			for (int level = bottomLevel + 1; level <= batch_maxLevel; level++)
+			{
+				/* we have to look for the node with the appropiate level again
+				 * but not inside the next while loop
+				 */
+				next = 0;
+				while (next < n && nnode[next]->level < level)
+				{
+					next++;
+				}
+
+				while (true)
+				{
+					pred = preds[level];
+					succ = succs[level];
+					if (pred->next[level].compareAndSet(succ, nnode[next], false, false))
+					{
+						break;
+					}
+					findNode(data[count], preds, succs);
+				}
+			}
+
+			count += n;
+			inserted += n;
+
+			delete [] nnode;
+		}
+	}
+
+	delete [] preds;
+	delete [] succs;
+	delete [] batch_topLevel;
+
+	mSize += inserted;
+	return (inserted);
+}
 
 template<class T, class Comparator>
 bool SkipList<T,Comparator>::remove(const T& data)
 {
 	int bottomLevel = 0;
-	Node<T>** preds = new Node<T>*[MAX_LEVEL+1];
-	Node<T>** succs = new Node<T>*[MAX_LEVEL+1];
-	Node<T>* succ;
+	Node<T>** preds = new Node<T>*[MAX_LEVEL];
+	Node<T>** succs = new Node<T>*[MAX_LEVEL];
+	Node<T>* succ = nullptr;
 	bool marked = false;
 	
-	while(true) //This loop makes no sense
+	while (true) //This loop makes no sense
 	{
 		bool found = findNode(data, preds, succs);
-		if(!found)
+		if (!found)
 		{
+			delete[] preds;
+			delete[] succs;
 			return false;
 		}
 		else
 		{
 			Node<T>* node2rm = succs[bottomLevel];
 			
-			//TODO this should be an atomic operation
-			//marked = node2rm->next[bottomLevel].getMarked();
-			//succ = node2rm->next[bottomLevel].getRef();
-			while(true) //!marked
+			while (true) //!marked
 			{
 				bool iMarkedIt = node2rm->next[bottomLevel].compareAndSet(succ, succ, false, true);
 				//bool iMarkedIt = node2rm->next[bottomLevel].setMark(succ);
@@ -272,12 +655,12 @@ bool SkipList<T,Comparator>::remove(const T& data)
 				if (iMarkedIt)
 				{
 					// Now mark upper levels
-					for(int level = node2rm->level; level >= bottomLevel+1; level--)
+					for (int level = node2rm->level; level >= bottomLevel+1; level--)
 					{
 					marked = node2rm->next[level].getMarked();
 					succ = node2rm->next[level].getRef();
 				
-						while(!marked)
+						while (!marked)
 						{
 							node2rm->next[level].setMark(succ); //TODO this should simply return success
 							marked = node2rm->next[level].getMarked();
@@ -287,10 +670,14 @@ bool SkipList<T,Comparator>::remove(const T& data)
 					}
 					findNode(data, preds, succs); //is this for cleanup?? hangs without it
 					mSize--;
+					delete[] preds;
+					delete[] succs;
 					return true;
 				}
 				else if (marked)
 				{
+					delete[] preds;
+					delete[] succs;
 					return false;
 				}
 				marked = node2rm->next[bottomLevel].getMarked();
@@ -325,12 +712,12 @@ bool SkipList<T,Comparator>::pop_front(T& data)
 			if (iMarkedIt)
 			{
 				//Now mark upper levels
-				for(int level = curr->level; level >= bottomLevel+1; level--)
+				for (int level = curr->level; level >= bottomLevel+1; level--)
 				{
 					marked = curr->next[level].getMarked();
 					succ = curr->next[level].getRef();
 				
-					while(!marked)
+					while (!marked)
 					{
 						curr->next[level].setMark(succ); //TODO this should simply return success
 						marked = curr->next[level].getMarked();
@@ -353,7 +740,7 @@ bool SkipList<T,Comparator>::pop_front(T& data)
 	} // currptr != null
 	// reached end of skip list
 	return false;
-};
+}
 
 template<class T, class Comparator>
 size_t SkipList<T,Comparator>::pop_front(T data[], int k)
@@ -364,6 +751,10 @@ size_t SkipList<T,Comparator>::pop_front(T data[], int k)
 	Node<T>* succ = nullptr;
 	int count = 0;
 
+	if (k == 0)
+	{
+		return 0;
+	}
 	curr = head->next[bottomLevel].getRef();
 	while (curr != nullptr)
 	{
@@ -381,18 +772,17 @@ size_t SkipList<T,Comparator>::pop_front(T data[], int k)
 			if (iMarkedIt)
 			{
 				//Now mark upper levels
-				for(int level = curr->level; level >= bottomLevel+1; level--)
+				for (int level = curr->level; level >= bottomLevel+1; level--)
 				{
 					marked = curr->next[level].getMarked();
 					succ = curr->next[level].getRef();
 				
-					while(!marked)
+					while (!marked)
 					{
 						curr->next[level].setMark(succ); //TODO this should simply return success
 						marked = curr->next[level].getMarked();
 						succ = curr->next[level].getRef();
 					}
-				
 				}
 
 				// return data
@@ -428,7 +818,7 @@ void SkipList<T,Comparator>::print()
 		p = p->next[0].getRef();
 	}
 	std::cout << "---------------END-----------------------" << std::endl;
-};
+}
 
 /*template<class T, class Comparator>
 void SkipList<T,Comparator>::printLevel(int l)
@@ -436,11 +826,11 @@ void SkipList<T,Comparator>::printLevel(int l)
 	Node<T> *p;
 	// the skip list is traversed by level
 	p = head->next[l];
-	while(p != nullptr)
+	while (p != nullptr)
 	{
 		cout << p->data << endl;
 		p = p->next[l];
 	}
-};*/
+}*/
 
 #endif
--- ppq_tbb/main.cpp
+++ ppq_tbb/main.cpp
@@ -6,7 +6,6 @@
 
 using namespace std;
 using namespace tbb;
-//namespace po = boost::program_options;
 
 #define THREADS 2
 #define N 1000000
@@ -80,46 +79,6 @@ const option::Descriptor usage[] =
 int readCmdLine(int argc, char** argv, bool &benchEn, bool &pop, bool &rm, int &numThreads, int &numInserts, float &fixInserts, bool &verbose, bool &lock_free, bool &tbb)
 {
 	// program options
-/*	po::options_description desc;
-	desc.add_options()
-		("help,h", "produce help message")
-		("benchmark,b", po::bool_switch(&benchEn)->default_value(false), "run benchmarks")
-		("pop_front,p", po::bool_switch(&pop)->default_value(false), "benchmark pop_front method")
-		("remove,r", po::bool_switch(&rm)->default_value(false), "benchmark remove method")
-		("threads,t", po::value<int>(&numThreads)->default_value(1), "number of threads")
-		("# inserts,i", po::value<int>(&numInserts)->default_value(1), "number of insert operations")
-		("\% fixed inserts,f", po::value<float>(&fixInserts)->default_value(1), "percentage of fixed inserts")
-		("verbose,v", po::bool_switch(&verbose)->default_value(false), "print extra messages")
-		("lock_free,l", po::bool_switch(&lock_free)->default_value(false), "use lock_free ppq implementation")
-		("concurrent_ppq_tbb,c", po::bool_switch(&tbb)->default_value(false), "use tbb ppq implementation")
-		;
-	po::variables_map vm;
-	try
-	{
-		po::store(po::parse_command_line(argc, argv, desc), vm); // can throw
-		// help option
-		if (vm.count("help"))
-		{
-			cout << desc << endl;
-			return 1;
-		}
-		po::notify(vm);
-	}
-	catch(po::error& e)
-	{
-		cerr << "ERROR:" << e.what() << endl << endl;
-		cerr << desc << endl;
-		return 1;
-	}*/
-	return 0;
-}
-
-
-int main(int argc, char** argv) {
-
-	bool benchEn = false, pop = false, rm = false, verbose = false, lock_free = false, tbb = false;
-	int numThreads = 2, numInserts = 100;
-	float fixInserts = 1;
 	stringstream strValue, strVal;
 	argc-=(argc>0); argv+=(argc>0); // skip program name argv[0] if present
 	option::Stats stats(usage, argc, argv);
@@ -179,11 +138,18 @@ int main(int argc, char** argv) {
 				break;
 		}
 	}
+	return 0;
+}
 
 
-		
+int main(int argc, char** argv) {
 
+	bool benchEn = false, pop = false, rm = false, verbose = false, lock_free = false, tbb = false;
+	int numThreads = 2, numInserts = 100;
+	float fixInserts = 1;
 
+	if (!readCmdLine(argc, argv, benchEn, pop, rm, numThreads, numInserts, fixInserts, verbose, lock_free, tbb))
+	{
 		if (benchEn)
 		{
 		cout << "Running only with " << THREADS << " threads " << endl;
@@ -203,6 +169,6 @@ int main(int argc, char** argv) {
 		benchmark(true, rm, numThreads, numInserts, fixInserts, verbose);
 		else if (lock_free)
 		lf_benchmark(true, rm, numThreads, numInserts, fixInserts, verbose);
-		
+	}
 	return 0;
 }
