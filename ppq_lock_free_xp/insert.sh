04b5ee92b1ca8a509887cb6aa7132666bee5eadc
David Sidler <david.sidler@gmail.com>
2014-12-10 13:50:08 +0100

    Updating Micfile & inser.sh

---
+++ ppq_lock_free_xp/insert.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#SBATCH --gres=mic:1 -n 1 -N 1
+export SINK_LD_LIBRARY_PATH=/opt/intel/composer_xe_2015/lib/mic/
+export SINK_LD_LIBRARY_PATH=$SINK_LD_LIBRARY_PATH:/opt/intel/tbb/lib/mic/
+srun --gres=mic:1 micnativeloadex ./main -a "-p -i 41666 -t 240 -f 0.5"
